47a42fe35fdf57c4a5daf9d107edbb27e4c41681
migueldf10 <migueldf10@gmail.com>
2020-06-16 16:26:25 +0200

    Added body to model as well

--- models/message.js
+++ models/message.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const message = sequelize.define('message', {
-
+    body: DataTypes.STRING
   }, {});
   message.associate = function (models) {
     message.belongsTo(models.user);
